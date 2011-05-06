c6674cdfa24734e463d190589720dca5143cad21
Rohit Trivedi <86.rohit@gmail.com>
2011-05-06 11:18:10 +0530

    line item controller create method

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ActiveRecord::Base
   default_scope :order  => 'title'
+  has_many :line_items
+  
+  before_destroy :ensure_not_referenced_by_any_line_item
   
   validates :title, :description, :image_url, :presence  => true
   validates :title, :length  => {:minimum  => 10}
@@ -9,4 +12,15 @@ class Product < ActiveRecord::Base
     :with  =>  %r{\.(gif|jpg|png)$}i,
     :message  => 'must be url for gif, jpg or png image'
   }
+  
+  #ensure there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    if line_items.count.zero?
+      return true
+    else
+      errors[:base] << "Line Items present"
+      return false
+    end
+  end
+
 end
