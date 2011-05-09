d08a8ecb6426ca3e45d9a1d65bc4628cf601ee59
Rohit Trivedi <86.rohit@gmail.com>
2011-05-09 10:12:32 +0530

    Before partials and ajax implementation

--- test/functional/carts_controller_test.rb
+++ test/functional/carts_controller_test.rb
@@ -44,6 +44,13 @@ class CartsControllerTest < ActionController::TestCase
       delete :destroy, :id => @cart.to_param
     end
 
-    assert_redirected_to carts_path
+    assert_redirected_to store_path
+  end
+  
+  test "should destroy cart" do 
+    assert_difference('Cart.count', -1) do
+      delete :destroy, :id => @cart.to_param
+    end
+    assert_redirected_to store_path
   end
 end
